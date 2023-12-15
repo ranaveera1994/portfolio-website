f643b0c51d84aab7c3b775fa99dd2ebf2893af3f
Ranaveera Reddy Sirigi Reddy <resirigi@amazon.com>
2023-12-15 00:40:00 -0600

    website dev begins

---
+++ src/components/Body.js
@@ -0,0 +1,19 @@
+import '../App.css';
+
+function Body() {
+  return (
+    <div>
+      <h1>Ranaveera</h1>
+      <p>This is going to be my new portfolio website.</p>
+      <p>Watch out for more updates!!!</p>
+      <p>Programming Languages I know:</p>
+      <ul>
+        <li>python</li>
+        <li>HTML, CSS (lol...)</li>
+      </ul>
+
+    </div>
+  );
+}
+
+export default Body;
